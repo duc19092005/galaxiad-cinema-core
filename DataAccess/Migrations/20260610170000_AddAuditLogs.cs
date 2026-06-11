48c0d5b24493a5d2ee69c18469e64ec54d69f3f1
duc19092005 <duc19092005d@gmail.com>
2026-06-11 10:14:25 +0700

    fix(backend): wrap AuditLogEntity creation in conditional checks to prevent SQL migrations crash

--- DataAccess/Migrations/20260610170000_AddAuditLogs.cs
+++ DataAccess/Migrations/20260610170000_AddAuditLogs.cs
@@ -13,59 +13,41 @@ namespace DataAccess.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "AuditLogEntity",
-                columns: table => new
-                {
-                    AuditLogId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Action = table.Column<string>(type: "varchar(50)", nullable: false),
-                    EntityType = table.Column<string>(type: "varchar(80)", nullable: false),
-                    EntityId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    EntityName = table.Column<string>(type: "nvarchar(300)", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(1000)", nullable: false),
-                    ActorUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    ActorName = table.Column<string>(type: "nvarchar(100)", nullable: false),
-                    ActorPrimaryRole = table.Column<string>(type: "varchar(50)", nullable: false),
-                    IsAdminAction = table.Column<bool>(type: "bit", nullable: false),
-                    CinemaId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_AuditLogEntity", x => x.AuditLogId);
-                    table.ForeignKey(
-                        name: "FK_AuditLogEntity_UserInfoEntity_ActorUserId",
-                        column: x => x.ActorUserId,
-                        principalTable: "UserInfoEntity",
-                        principalColumn: "UserId",
-                        onDelete: ReferentialAction.Restrict);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_AuditLogEntity_ActorUserId",
-                table: "AuditLogEntity",
-                column: "ActorUserId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_AuditLogEntity_CinemaId",
-                table: "AuditLogEntity",
-                column: "CinemaId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_AuditLogEntity_CreatedAt",
-                table: "AuditLogEntity",
-                column: "CreatedAt");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_AuditLogEntity_EntityType_EntityId",
-                table: "AuditLogEntity",
-                columns: new[] { "EntityType", "EntityId" });
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AuditLogEntity]') AND type in (N'U'))
+                BEGIN
+                    CREATE TABLE [dbo].[AuditLogEntity] (
+                        [AuditLogId] uniqueidentifier NOT NULL,
+                        [Action] varchar(50) NOT NULL,
+                        [EntityType] varchar(80) NOT NULL,
+                        [EntityId] uniqueidentifier NULL,
+                        [EntityName] nvarchar(300) NOT NULL,
+                        [Description] nvarchar(1000) NOT NULL,
+                        [ActorUserId] uniqueidentifier NOT NULL,
+                        [ActorName] nvarchar(100) NOT NULL,
+                        [ActorPrimaryRole] varchar(50) NOT NULL,
+                        [IsAdminAction] bit NOT NULL,
+                        [CinemaId] uniqueidentifier NULL,
+                        [CreatedAt] datetime2 NOT NULL,
+                        CONSTRAINT [PK_AuditLogEntity] PRIMARY KEY ([AuditLogId]),
+                        CONSTRAINT [FK_AuditLogEntity_UserInfoEntity_ActorUserId] FOREIGN KEY ([ActorUserId]) REFERENCES [dbo].[UserInfoEntity] ([UserId])
+                    );
+                    CREATE INDEX [IX_AuditLogEntity_ActorUserId] ON [dbo].[AuditLogEntity] ([ActorUserId]);
+                    CREATE INDEX [IX_AuditLogEntity_CinemaId] ON [dbo].[AuditLogEntity] ([CinemaId]);
+                    CREATE INDEX [IX_AuditLogEntity_CreatedAt] ON [dbo].[AuditLogEntity] ([CreatedAt]);
+                    CREATE INDEX [IX_AuditLogEntity_EntityType_EntityId] ON [dbo].[AuditLogEntity] ([EntityType], [EntityId]);
+                END
+            ");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "AuditLogEntity");
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AuditLogEntity]') AND type in (N'U'))
+                BEGIN
+                    DROP TABLE [dbo].[AuditLogEntity];
+                END
+            ");
         }
     }
 }
